19f25e88479c77950098300c905ab01eed41529e
Debeing <mbaitelameric@gmail.com>
2025-07-23 13:18:11 +0100

    Initialisation

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,6 +1,15 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
-import { Moon } from "lucide-react";
+import { LogOut, Moon, Settings, SquareMenu, User } from "lucide-react";
 import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./button";
 
 const Navbar = () => {
   return (
@@ -11,10 +20,49 @@ const Navbar = () => {
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
         <Moon />
-        <Avatar>
-          <AvatarImage className="rounded-full w-8 h-8" src="https://avatars.githubusercontent.com/u/1486366" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage
+                className="rounded-full w-8 h-8"
+                src="https://avatars.githubusercontent.com/u/1486366"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <SquareMenu />
+              <span className="sr-only">Open Menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
